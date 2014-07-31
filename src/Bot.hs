ed50f20e4d710008c33d15354eb9c24316745079
Benjamin Kovach <bkovach13@gmail.com>
2014-07-31 14:54:15 -0400

    YEEEEAAH it works

--- src/Bot.hs
+++ src/Bot.hs
@@ -5,8 +5,10 @@ import Data.Maybe (fromJust)
 import Web.Authenticate.OAuth
 import Network.HTTP.Client
 import Network.HTTP.Client.TLS
+import Network.HTTP.Types
 import Control.Monad.IO.Class
 import Data.ByteString
+import Control.Monad
 
 oauthTwitter :: ByteString -> ByteString -> OAuth
 oauthTwitter key secret = 
@@ -21,89 +23,23 @@ oauthTwitter key secret =
            }
 
 auth :: OAuth
-auth = oauthTwitter "k1Gd4nLdXLXgHx1HwQ2S05F3J" "gi75TE8dJHqZJS1l8yBWkYI7wIx2fl616c4nEFXASaaFrA8yPL"
+auth = oauthTwitter "I2u0NYuLXMP1WKjS3384qe0vG" 
+                    "cRvdQMS3WI2POJfN8Y4T4cqMe2vrg32kCn2l8DXijQz7ihFcrF"
 
 credential :: Credential
-credential = newCredential "436362160-dCjYnNn7lEVHVNAnMSaTPzWMMbMwpqQ9S25k2ZZC" "jtoqYgGcNG3MMdo6lEqSgXarbqeCFEtJRjMGBN3gSc1GX"
+credential = newCredential "436362160-8mEQeSa3lfZV6L6KNqIgtO1X8R9Aq8JjDUaW8Mem" 
+                           "thWcMDsYz1gxV1laSO95RGmRa4NZMx9pHuYNG2bfuKZ30"
 
 --withOAuth :: MonadIO m => Request -> m Request
 withOAuth = signOAuth auth credential
 
--- req :: (MonadIO m, MonadThrow m) => m Request
 req = do
-  url <- parseUrl "https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=Bendotk"
-  withOAuth url
-
-mgr = newManager defaultManagerSettings
+  url <- parseUrl "https://api.twitter.com/1.1/statuses/update.json?status=TESTING"
+  withOAuth url{ method = "POST" }  
 
 testGo = do
   manager <- newManager tlsManagerSettings
   request <- req
   res     <- httpLbs request manager
-  print res
-
---{-# LANGUAGE OverloadedStrings, DeriveGeneric #-}
-
---import Data.ByteString (ByteString)
---import Network.HTTP.Client
---import Web.Authenticate.OAuth
---import Data.Aeson
---import Data.Time.Clock (UTCTime)
---import Data.Text (Text)
---import GHC.Generics
-
----- Insert here your own credentials
-
---myoauth :: OAuth
---myoauth =
---  newOAuth { oauthServerName     = "api.twitter.com"
---           , oauthConsumerKey    = "k1Gd4nLdXLXgHx1HwQ2S05F3J"
---           , oauthConsumerSecret = "gi75TE8dJHqZJS1l8yBWkYI7wIx2fl616c4nEFXASaaFrA8yPL"
---           }
-
---mycred :: Credential
---mycred = newCredential "436362160-dCjYnNn7lEVHVNAnMSaTPzWMMbMwpqQ9S25k2ZZC" "jtoqYgGcNG3MMdo6lEqSgXarbqeCFEtJRjMGBN3gSc1GX"
-
----- | Type for tweets. Use only the fields you are interested in.
-----   The parser will filter them. To see a list of available fields
-----   see <https://dev.twitter.com/docs/platform-objects/tweets>.
---data Tweet =
---  Tweet { text :: !Text
---        , created_at :: !UTCTime
---          } deriving (Show, Generic)
-
---instance FromJSON Tweet
---instance ToJSON Tweet
-
----- | This function reads a timeline JSON and parse it using the 'Tweet' type.
---timeline :: String -- ^ Screen name of the user
---         -> IO (Either String [Tweet]) -- ^ If there is any error parsing the JSON data, it
---                                       --   will return 'Left String', where the 'String'
---                                       --   contains the error information.
---timeline name = do
---  -- Firstly, we create a HTTP request with method GET (it is the default so we don't have to change that).
---  req <- parseUrl $ "https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=" ++ name
---  -- Using a HTTP manager, we authenticate the request and send it to get a response.
---  manager <- newManager defaultManagerSettings
---  res <- withManager defaultManagerSettings $ \m -> do
---           -- OAuth Authentication. 'signOAuth' modifies the HTTP header adding the
---           -- appropriate authentication.
---           signedreq <- signOAuth myoauth mycred req
---           -- Send request.
---           httpLbs signedreq m
---  -- Decode the response body.
---  return $ eitherDecode $ responseBody res
-
----- | The main function, as an example of how to use the 'timeline'
-----   function.
---main :: IO ()
---main = do
---  -- Read the timeline from Hackage user. Feel free to change the screen
---  -- name to any other.
---  ets <- timeline "Hackage"
---  case ets of
---   -- When the parsing of the JSON data fails, we report it.
---   Left err -> putStrLn err
---   -- When successful, print in the screen the first 5 tweets.
---   Right ts  -> mapM_ print $ take 5 ts
+  print $ responseBody res
 
