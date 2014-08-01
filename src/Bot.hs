8888407fa2c8eb43b4c5151aeb911dc249a9af1c
Benjamin Kovach <bkovach13@gmail.com>
2014-08-01 09:03:45 -0400

    Configuration!

--- src/Bot.hs
+++ src/Bot.hs
@@ -1,5 +1,5 @@
-{-# LANGUAGE OverloadedStrings, NoMonomorphismRestriction #-}
-module Bot(tweet) where
+{-# LANGUAGE OverloadedStrings, NoMonomorphismRestriction, DeriveGeneric, RecordWildCards #-}
+module Bot where
 
 import Data.Maybe (fromJust)
 import Web.Authenticate.OAuth
@@ -7,10 +7,23 @@ import qualified Network.HTTP.Base as HTTP
 import Network.HTTP.Client
 import Network.HTTP.Client.TLS
 import Network.HTTP.Types
-import Control.Monad.IO.Class
 import Data.ByteString
+import qualified Data.ByteString.Char8 as B
 import Control.Monad
-import qualified Data.ByteString.Lazy.Internal as BL
+import qualified Data.ByteString.Lazy as BL
+import Data.Aeson
+import GHC.Generics
+
+
+data Config = Config {
+    oauthKey :: String,
+    oauthSecret :: String,
+    appKey :: String,
+    appSecret :: String
+  } deriving (Show, Generic)
+
+instance FromJSON Config
+instance ToJSON Config
 
 oauthTwitter :: ByteString -> ByteString -> OAuth
 oauthTwitter key secret = 
@@ -32,22 +45,30 @@ credential :: Credential
 credential = newCredential "2696415289-pRPx0TyuFZW7BjjHTDiX2g13qn8vl9N0QZJSzVP" 
                            "B1FnzuF4Yodb2g7qr8bDmuJMieXHgCaxXLhX4BgM5gyjj"
 
---withOAuth :: MonadIO m => Request -> m Request
+withOAuth :: Request -> IO Request
 withOAuth = signOAuth auth credential
 
-req = do
-  url <- parseUrl "https://api.twitter.com/1.1/statuses/update.json?status=TESTING"
-  withOAuth url{ method = "POST" }
+signWithConfig :: Config -> Request -> IO Request
+signWithConfig Config{..} = signOAuth 
+  (oauthTwitter (B.pack oauthKey) (B.pack oauthSecret)) 
+  (newCredential (B.pack appKey) (B.pack appSecret))
 
-testGo = do
-  manager <- newManager tlsManagerSettings
-  request <- req
-  res     <- httpLbs request manager
-  print $ responseBody res
+configFromFile :: FilePath -> IO (Either String Config)
+configFromFile path = do
+  contents <- BL.readFile path
+  return $ eitherDecode contents 
 
+mkStatusReq :: String -> IO Request
 mkStatusReq status = do
   url <- parseUrl $ "https://api.twitter.com/1.1/statuses/update.json?status=" ++ HTTP.urlEncode status
   withOAuth url{ method = "POST" }
+
+tweetWithConfig :: Config -> String -> IO (Response BL.ByteString)
+tweetWithConfig config status = do
+  url <- parseUrl $ "https://api.twitter.com/1.1/statuses/update.json?status=" ++ HTTP.urlEncode status
+  req <- signWithConfig config url{ method = "POST" }
+  manager <- newManager tlsManagerSettings
+  httpLbs req manager
 
 tweet :: String -> IO (Response BL.ByteString)
 tweet status = do
--- src/RapCandy.hs
+++ src/RapCandy.hs
@@ -1,8 +1,10 @@
 module Main where
 
-import Data.Text as T
+import qualified Data.Text as T
 import Control.Concurrent (threadDelay)
 import Control.Monad(forever)
+import System.Environment(getArgs)
+import Control.Applicative
 
 import Markov
 import Tweet
@@ -14,7 +16,19 @@ minute = 60 * second
 hour   = 60 * minute
 
 main :: IO ()
-main = forever $ do
-  status <- randomTweet
-  tweet (T.unpack status)
-  threadDelay (30 * minute)
+main = do 
+  argPairs <- zip <*> tail <$> getArgs
+  case argPairs of
+    [] -> error "Usage: RapCandy -c <path/to/config.json>"
+    _  -> return ()
+  let conf = lookup "-c" argPairs
+  case conf of
+    Nothing -> error "Please pass filepath to config.json as argument to -c" 
+    Just path -> do
+      cfg <- configFromFile path
+      case cfg of 
+        Left _ -> error "Trouble parsing configuration file."
+        Right config -> forever $ do
+          status <- randomTweet
+          tweetWithConfig config (T.unpack status)
+          threadDelay (30 * minute)
